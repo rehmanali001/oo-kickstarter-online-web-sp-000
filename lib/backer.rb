8bf0e5848ffb03e3f90e434462f646082eee26ad
Rehman Ali <rehmanali001@gmail.com>
2019-08-13 19:46:00 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,9 +1,8 @@
 class Backer 
-    attr_reader :name
+    attr_reader :name, :backed_projects
   
   def initialize(name)
     @name = name 
-            @backed_projects = []
-
+    @backed_projects = []
   end
 end 
